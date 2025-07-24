cb9dd0956655a8f6f0725d7b1a71512a62e84bd7
田中郁也 <fumiyatanaka@tanakaikuyanoMacBook-Pro.local>
2025-07-24 22:51:36 +0900

    Fix: ファイル内容を正しく設定 - ツブッターサービス完成

---
+++ src/App.vue
@@ -0,0 +1,1 @@
+<script setup>import MainApp from "./components/MainApp.vue"</script><template><div id="app"><MainApp /></div></template><style scoped>#app { width: 100%; min-height: 100vh; }</style>
